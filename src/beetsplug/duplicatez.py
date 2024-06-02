713638a3a20f1dee290d0615a54bb761868c0c02
David Trattnig <david@subsquare.at>
2024-06-02 19:43:43 +0200

    style: format

--- src/beetsplug/duplicatez.py
+++ src/beetsplug/duplicatez.py
@@ -26,18 +26,19 @@ class DuplicatezPlugin(DuplicatesPlugin):
         self._count_tracks = 0
         self._count_dups = 0
         self._file = "./output/beets-duplicates.json"
+
         # Purposely reuse 'duplicates' config
-        self.config = beets.config["duplicates"]       
+        self.config = beets.config["duplicates"]
 
     def commands(self) -> list:
         """Wrap the parent method and call JSON rendering."""
         super().commands()
         inner_func = self._command.func
+
         def wrapper_func(lib, opts, args):
-            
             inner_func(lib, opts, args)
             self._render_json()
-            
+
         self._command.func = wrapper_func
         return [self._command]
 
@@ -45,7 +46,7 @@ class DuplicatezPlugin(DuplicatesPlugin):
         self, item: Item, copy=False, move=False, delete=False, tag=False, fmt=""
     ):
         """Wraps the parent method and creates an additional duplicates dict."""
-        super()._process_item(item, copy, move, delete, tag, fmt)       
+        super()._process_item(item, copy, move, delete, tag, fmt)
         key = item.get("mb_trackid")
         key_format = "$mb_trackid:$mb_albumid"
         key = format(item, key_format)
@@ -53,11 +54,11 @@ class DuplicatezPlugin(DuplicatesPlugin):
         dup = str(format(item, "$path"))
         self._dupz.get(key).append(dup)
         self._count_dups += 1
-    
+
     def _render_json(self):
         """Renders the JSON file."""
         self._count_tracks = len(self._dupz.keys())
-        with open(self._file, "w") as file: 
+        with open(self._file, "w") as file:
             json.dump(self._dupz, file, indent=4)
         print("---")
         print(f"Found {self._count_tracks} tracks with {self._count_dups} duplicates")
