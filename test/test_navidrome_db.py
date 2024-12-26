bb2b7d4f9e2f6afe6d5f4074470d8106f6237c2f
David Trattnig <david@subsquare.at>
2024-12-26 15:40:26 +0100

    test: extend pytest test cases

--- test/test_navidrome_db.py
+++ test/test_navidrome_db.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import pytest
 from ndtools.db import NavidromeDb, Annotation
 from ndtools.utils import DotDict
 
@@ -31,43 +31,47 @@ test_anno = DotDict(
 )
 
 
-class TestNavidromeDb(TestCase):
-    def setUp(self) -> None:
-        self.db = NavidromeDb(db_path=TEST_DB_PATH)
+@pytest.fixture(scope="session")
+def db():
+    db = NavidromeDb(db_path=TEST_DB_PATH)
+    yield db
 
-    def test_init_user(self):
-        # Verify that the user ID is correctly initialized
-        assert self.db.user_id == TEST_USER_ID
 
-    def test_generate_annotation_id(self):
-        # Verify that generated IDs are strings and have the expected format
-        annotation_id = NavidromeDb.generate_annotation_id()
-        assert isinstance(annotation_id, str)
-        assert len(annotation_id) == 36
+def test_init_user(db):
+    # Verify that the user ID is correctly initialized
+    assert db.user_id == TEST_USER_ID
 
-    def test_get_media(self):
-        db = NavidromeDb(TEST_DB_PATH)
-        media_file = db.get_media(test_media_file.path)
 
-        print(f"Media file: {media_file}")
-        print(f"File annotation: {media_file.annotation}")
-        print(f"Artist annotation: {media_file.artist.annotation}")
-        print(f"Album annotation: {media_file.album.annotation}")
+def test_generate_annotation_id():
+    # Verify that generated IDs are strings and have the expected format
+    annotation_id = NavidromeDb.generate_annotation_id()
+    assert isinstance(annotation_id, str)
+    assert len(annotation_id) == 36
 
-        assert media_file is not None
-        assert media_file.id == test_media_file.id
-        assert media_file.path == test_media_file.path
-        assert media_file.title == test_media_file.title
-        assert media_file.year == test_media_file.year
-        assert media_file.track_number == test_media_file.track_number
-        assert media_file.duration == test_media_file.duration
-        assert media_file.bitrate == test_media_file.bitrate
 
-        anno: Annotation = media_file.annotation
-        assert anno.id == test_anno.id
-        assert anno.item_id == test_anno.item_id
-        assert anno.play_count == test_anno.play_count
-        assert anno.play_date == test_anno.play_date
-        assert anno.rating == test_anno.rating
-        assert anno.starred == test_anno.starred
-        assert anno.starred_at is test_anno.starred_at
+def test_get_media(db):
+    db = NavidromeDb(TEST_DB_PATH)
+    media_file = db.get_media(test_media_file.path)
+
+    print(f"Media file: {media_file}")
+    print(f"File annotation: {media_file.annotation}")
+    print(f"Artist annotation: {media_file.artist.annotation}")
+    print(f"Album annotation: {media_file.album.annotation}")
+
+    assert media_file is not None
+    assert media_file.id == test_media_file.id
+    assert media_file.path == test_media_file.path
+    assert media_file.title == test_media_file.title
+    assert media_file.year == test_media_file.year
+    assert media_file.track_number == test_media_file.track_number
+    assert media_file.duration == test_media_file.duration
+    assert media_file.bitrate == test_media_file.bitrate
+
+    anno: Annotation = media_file.annotation
+    assert anno.id == test_anno.id
+    assert anno.item_id == test_anno.item_id
+    assert anno.play_count == test_anno.play_count
+    assert anno.play_date == test_anno.play_date
+    assert anno.rating == test_anno.rating
+    assert anno.starred == test_anno.starred
+    assert anno.starred_at is test_anno.starred_at
--- test/test_processor.py
+++ test/test_processor.py
@@ -1,4 +1,5 @@
-from unittest import TestCase
+import pytest
+
 from ndtools.db import NavidromeDb
 from ndtools.list import DuplicateProcessor
 from ndtools.model import Annotation, MediaFile
@@ -7,14 +8,77 @@ TEST_DB_PATH = "config/navidrome/navidrome.db"
 DIR_OUTPUT = "./output"
 
 
-class TestProcessor(TestCase):
-    def setUp(self):
-        db = NavidromeDb(TEST_DB_PATH)
-        self.processor = DuplicateProcessor(db, DIR_OUTPUT)
+@pytest.fixture(scope="session")
+def db():
+    db = NavidromeDb(db_path=TEST_DB_PATH)
+    yield db
 
-    def test_merge_annotation_data(self):
-        # Create two MediaFile objects with some annotation data
-        a = MediaFile(
+
+@pytest.fixture(scope="session")
+def processor():
+    processor = DuplicateProcessor(db, DIR_OUTPUT)
+    yield processor
+
+
+def test_merge_annotation_data(processor):
+    # Create two MediaFile objects with some annotation data
+    a = MediaFile(
+        id="1",
+        path="/path/to/file1.mp3",
+        title="File 1",
+        year=1993,
+        track_number=3,
+        duration=3600,
+        bitrate=320,
+        artist_id=0,
+        album_id=0,
+    )
+    a.annotation = Annotation(
+        id="1a",
+        item_id="1",
+        item_type=Annotation.Type.media_file,
+        play_count=10,
+        play_date="2023-01-01",
+        rating=5,
+        starred=True,
+        starred_at="2022-01-01",
+    )
+
+    b = MediaFile(
+        id="2",
+        path="/path/to/file2.mp3",
+        title="File 2",
+        year=1993,
+        track_number=3,
+        duration=3600,
+        bitrate=320,
+        artist_id=0,
+        album_id=0,
+    )
+    b.annotation = Annotation(
+        id="2",
+        item_id="2",
+        item_type=Annotation.Type.media_file,
+        play_count=5,
+        play_date="2023-01-02",
+        rating=4,
+        starred=False,
+        starred_at=None,
+    )
+
+    # Call the merge_annotation_data method
+    processor._merge_annotation_data(a, b)
+    assert a.annotation.play_count == 15
+    assert a.annotation.play_date == "2023-01-02"
+    assert a.annotation.rating == 5
+    assert a.annotation.starred
+    assert b.annotation.starred
+
+
+def test_merge_annotations(processor):
+    # Create a list of four Media files with annotations
+    files = [
+        MediaFile(
             id="1",
             path="/path/to/file1.mp3",
             title="File 1",
@@ -24,19 +88,8 @@ class TestProcessor(TestCase):
             bitrate=320,
             artist_id=0,
             album_id=0,
-        )
-        a.annotation = Annotation(
-            id="1a",
-            item_id="1",
-            item_type=Annotation.Type.media_file,
-            play_count=10,
-            play_date="2023-01-01",
-            rating=5,
-            starred=True,
-            starred_at="2022-01-01",
-        )
-
-        b = MediaFile(
+        ),
+        MediaFile(
             id="2",
             path="/path/to/file2.mp3",
             title="File 2",
@@ -46,26 +99,45 @@ class TestProcessor(TestCase):
             bitrate=320,
             artist_id=0,
             album_id=0,
-        )
-        b.annotation = Annotation(
-            id="2",
-            item_id="2",
+        ),
+        MediaFile(
+            id="3",
+            path="/path/to/file3.mp3",
+            title="File 3",
+            year=1993,
+            track_number=3,
+            duration=3600,
+            bitrate=320,
+            artist_id=0,
+            album_id=0,
+        ),
+        MediaFile(
+            id="4",
+            path="/path/to/file4.mp3",
+            title="File 4",
+            year=1993,
+            track_number=3,
+            duration=3600,
+            bitrate=320,
+            artist_id=0,
+            album_id=0,
+        ),
+    ]
+    # Set annotations for each file
+    for f in files:
+        f.annotation = Annotation(
+            id=f"a{f.id}",
+            item_id=f.id,
             item_type=Annotation.Type.media_file,
-            play_count=5,
-            play_date="2023-01-02",
-            rating=4,
-            starred=False,
-            starred_at=None,
+            play_count=f.id if int(f.id) > 1 else 0,  # File 1 has no play count, others have 2, 3 and 4 plays
+            play_date="2023-01-01",
+            rating=int(f.id),  # Ratings from 1 to 4
+            starred=f.id == "2" or f.id == "4",  # Files 2 and 4 are starred
+            starred_at="2022-01-01",
         )
 
-        # Call the merge_annotation_data method
-        self.processor.merge_annotation_data(a, b)
-        # Check if the annotation data has been merged correctly
-        self.assertEqual(a.annotation.play_count, 15)
-        # Check if the play_date is updated to the latest date
-        self.assertEqual(a.annotation.play_date, "2023-01-02")
-        # Check if the rating is updated to the highest rating
-        self.assertEqual(a.annotation.rating, 5)
-        # Check if both files are starred if either one was starred
-        self.assertTrue(a.annotation.starred)
-        self.assertTrue(b.annotation.starred)
+    processor.merge_annotations(files)
+
+    assert files[0].annotation.play_count == 9
+    assert files[0].annotation.rating == 4
+    assert files[0].annotation.starred
