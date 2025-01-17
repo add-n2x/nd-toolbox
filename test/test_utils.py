4f9e8c34dd0ab103eca27c11455c62e510e6f4d2
David Trattnig <david@subsquare.at>
2025-01-17 21:38:44 +0100

    test: date util

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,8 @@
 """Test utils module."""
 
-from ndtoolbox.utils import FileUtil
+from datetime import datetime
+
+from ndtoolbox.utils import DateUtil, FileUtil
 
 
 def test_file_name_string_suffix():
@@ -32,3 +34,13 @@ def test_is_album_folder():
     assert FileUtil.is_album_folder(base_path, album_path) is False
     album_path = "/path/to/other_base/artist_name/album_name"
     assert FileUtil.is_album_folder(base_path, album_path) is False
+
+
+def test_date_util():
+    """Test the date utility functions."""
+    now = datetime.now()
+    s = DateUtil.format_date(now)
+    assert isinstance(s, str)
+    assert s == now.strftime("%Y-%m-%d %H:%M:%S")
+    now2 = DateUtil.parse_date(s)
+    assert now.date() == now2.date()
