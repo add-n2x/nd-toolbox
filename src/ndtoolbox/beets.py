7dff2fa271a85ce4dd311c28b8f8567adedf4512
David Trattnig <david@subsquare.at>
2025-01-21 22:07:01 +0100

    refactor: draft

--- src/ndtoolbox/beets.py
+++ src/ndtoolbox/beets.py
@@ -1,16 +1,58 @@
 """Classes for interaction with Beets."""
 
+# import os
 import subprocess
+
+# from pathlib import Path
 from typing import Generator
 
+# import confuse
+# from beets.dbcore.query import AndQuery, FieldQuery, OrQuery, SubstringQuery
+# from beets.library import Library
 from easydict import EasyDict
 
+# from beets.ui import print_
+# from beets.util import syspath, displayable_path, normpath
+# from ndtoolbox import config
 from ndtoolbox.utils import PrintUtil as PU
 from ndtoolbox.utils import StringUtil as SU
 
 
 class BeetsClient:
     """Client wrapping commands for Beets."""
+
+    # lib: Library = None
+
+    # def __init__(self):
+    #     """Initialize the Beets client."""
+    #     confuse.Path(cwd="config/beets")
+    #     libpath = config["beets"]["library"].get(str)
+    #     BeetsClient.lib = Library(libpath)
+    #     paths_found = []
+
+    # def _query_lib(self, beet_query, message, silent=False):
+    #     """beet_query can be string or query object. Returns a list of paths."""
+    #     # libpath = os.path.expanduser(Path(SHELLENV["BEETSDIR"]) / "jtbeets_gin.db")
+    #     libpath = Config.beets_db_path
+    #     lib = Library(libpath)
+    #     paths_found = []
+
+    #     for item in lib.items(beet_query):
+    #         paths_found += [item.get("path")]
+    #     return paths_found
+
+    # def _query1(self):
+    #     isrc = ""
+    #     beet_result = self._query_lib(SubstringQuery("isrc", isrc), "", silent=True)
+    #     return beet_result
+
+    # def _query1(self):
+    #     artist = "oasis"
+    #     title = "wonderwall"
+    #     beet_result = self._query_lib(
+    #         AndQuery([SubstringQuery("artist", artist), SubstringQuery("title", title)]),
+    #     )
+    #     return beet_result
 
     @staticmethod
     def get_album_info(album_path) -> Generator[EasyDict]:
@@ -25,7 +67,7 @@ class BeetsClient:
                 Usually it only returns one record, but can return multiple records when the folder contains
                 files from multiple albums. In that case, it will be treated as a manual compilation (mixtape).
         """
-        album_info = EasyDict({"album": None, "total": None, "missing": None})
+        album_info = EasyDict({"album": None, "total": None, "missing": None, "compilation": False})
         cmd = f"beet ls -a -f '$album:::$albumtotal:::$missing:::$comp' path:\"{album_path}\""
         PU.debug(f"BEET CMD: {cmd}")
 
