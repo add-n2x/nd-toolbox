6039d52d98d60d53f70789b6782087844f82a3a5
David Trattnig <david@subsquare.at>
2025-01-25 22:27:36 +0100

    test: ms store python does not work with Beets

--- test/test_beets.py
+++ test/test_beets.py
@@ -4,17 +4,23 @@ Tests for the Beets integration.
 
 import io
 import sys
+from datetime import datetime
 
 from beets.ui import main
 
-from ndtoolbox.beets import beets
+from ndtoolbox.beets import BeetsClient
 from ndtoolbox.config import config
+
+IS_MICROSOFT_PYTHON = "AppData\\Local\\Microsoft\\WindowsApps" in sys.exec_prefix
 
 config.set_file("test/config/config.yaml")
 
 
 def test_beets_ui():
     """Test Beets UI."""
+    if IS_MICROSOFT_PYTHON:
+        return
+
     captured_output = io.StringIO()
     sys.stdout = captured_output
     main(["-c", "config/beets/config.yaml", "stats"])
@@ -26,13 +32,23 @@ def test_beets_ui():
 
 def test_beets_client():
     """Test Beets client."""
-    info = beets.get_album_info("/music/Calibre/Second Sun/")
+    if IS_MICROSOFT_PYTHON:
+        return
+
+    start1 = datetime.now()
+    info = BeetsClient(0).get_album_info("/music/Calibre/Second Sun/")
     assert info is not None
     for i in info:
         print("Subprocess Query - Beets album info: " + i)
+    stop1 = datetime.now()
 
-    # client = BeetsClient(1)
-    # info = client.get_album_info("/music/Calibre/Second Sun/")
-    # assert info is not None
-    # for i in info:
-    #     print("Stdio Capture - Beets album info: " + i)
+    start2 = datetime.now()
+    client = BeetsClient(1)
+    info = client.get_album_info("/music/Calibre/Second Sun/")
+    assert info is not None
+    for i in info:
+        print("Stdio Capture - Beets album info: " + i)
+    stop2 = datetime.now()
+
+    print("Subprocess Query Time: " + str(stop1 - start1))
+    print("Stdio Capture Time: " + str(stop2 - start2))
